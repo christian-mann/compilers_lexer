1d51309ff256881969690bdc50d01dbb836f6fd4
Christian Mann <christian-mann@utulsa.edu>
2012-08-28 10:49:33 -0500

    Changed bounds on line-reading code
    Remember, we have the line + newline + null-terminator

--- src/lexer.c
+++ src/lexer.c
@@ -1,9 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<stdbool.h>
-#include<file.h>
-
-#define NULL 0
+#include<string.h>
 
 int main(int argc, char **argv) {
 	//open the file given as an argument
@@ -12,21 +10,26 @@ int main(int argc, char **argv) {
 	if(argc > 1) {
 		sfSrc = argv[1];
 		if(!(fSrc = fopen(sfSrc, "r"))) {
-			fprintf("No source file found at %s", sfSrc);
+			fprintf(stderr, "Error: No source file found at %s\n", sfSrc);
+			return -1;
 		}
 	} else {
-		fprintf("Warning: No source file given; using stdin", stderr);
+		fprintf(stderr, "Warning: No source file given; using stdin\n");
 		fSrc = stdin;
 	}
 
 	//begin reading a line at a time
-	char sLine[73];
+	char sLine[74]; //72 characters per line + '\n' + '\0'
 	int cLine = 1;
-	while(fgets(sLine, sizeof(sLine)-1, fSrc) != NULL) {
+	while(fgets(sLine, sizeof(sLine), fSrc) != NULL) {
 		//strip trailing '\n' if it exists
 		int len = strlen(sLine) - 1;
-		if(sLine[len] == '\n')
+		if(sLine[len] == '\n') {
 			sLine[len] = 0;
+		} else {
+			fprintf(stderr, "Error: Line too long at line %d\n", cLine);
+			return -1;
+		}
 		printf("%d. %s\n", cLine, sLine);
 		cLine++;
 	}
