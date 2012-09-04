1ecb6832ff83f8d1524b8a1e093ce8cf95a658d8
Christian Mann <christian-mann@utulsa.edu>
2012-09-03 20:29:24 -0500

    Began the process of hooking up pipes to files
    I just know there will be twenty bugs in this code.

--- src/lexer.c
+++ src/lexer.c
@@ -7,25 +7,49 @@
 #include "machines.h"
 
 int main(int argc, char **argv) {
-	//open the file given as an argument
-	char *sfSrc;
-	FILE *fSrc;
-	if(argc > 1) {
-		sfSrc = argv[1];
-		if(!(fSrc = fopen(sfSrc, "r"))) {
-			fprintf(stderr, "Error: No source file found at %s\n", sfSrc);
-			return -1;
+	//open the files given as an argument
+	char *sfSrc, *sfListing, *sfToken, *sfReservedWords;
+	FILE *fSrc, *fListing, *fToken, *fReservedWords;
+	sfSrc = sfListing = sfToken = NULL;
+
+	// format: ./a.out -r sfReservedWords [-l sfListing] [-t sfToken] [[-s] sfSrc]
+	int i;
+	for(i = 0; i < argc; i++) {
+		if(!strcmp(argv[i], "-l")) {
+			sfListing = argv[i+1];
 		}
-	} else {
-		fprintf(stderr, "Warning: No source file given; using stdin\n");
+		if(!strcmp(argv[i], "-t")) {
+			sfToken = argv[i+1];
+		}
+		if(!strcmp(argv[i], "-s") || i == argc - 2) {
+			sfSrc = argv[i+1];
+		}
+		if(!strcmp(argv[i], "-r")) {
+			sfReservedWords = argv[i+1];
+		}
+	}
+	if(!sfSrc || (fSrc = fopen(sfSrc, "r")) == NULL) {
+		fprintf(stderr, "Warning: source file not given or not found, using stdin\n");
 		fSrc = stdin;
+	}
+	if(!sfToken || (fToken = fopen(sfToken, "w")) == NULL) {
+		fprintf(stderr, "Warning: token file not given or not found, using stdout\n");
+		fToken = stdout;
+	}
+	if(!sfReservedWords || (fReservedWords = fopen(sfReservedWords, "r")) == NULL) {
+		fprintf(stderr, "Warning: reserved word file not given or not found, not using reserved words\n");
 	}
 
 	//create symbol table
 	SymbolTable *symbtab = malloc(sizeof(SymbolTable));
 
 	//read in reserved word list
-	ReservedWordList *rwl = malloc(sizeof(ReservedWordList));
+	ReservedWordList *rwl;
+	if(fReservedWords) {
+		rwl = parseResWordFile(fReservedWords);
+	} else {
+		rwl = malloc(sizeof(ReservedWordList));
+	}
 
 	//begin reading a line at a time
 	char sLine[80];
