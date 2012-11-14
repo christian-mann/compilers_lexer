f5bb96a160b266fbc50232f173fe5dbec06752c8
Christian Mann <christian-mann@utulsa.edu>
2012-11-13 19:37:38 -0600

    Changed to more correct syntax

--- src/tests/sample-pascal.pas
+++ src/tests/sample-pascal.pas
@@ -1,18 +1,18 @@
 program example(input, output);
-var x, y: integer;
-function gcd(a, b: integer): integer;
+var x: integer;
+procedure gcd(a: integer);
 begin
-	if b = 1 then gcd := a
-	if b = 1234567890
-	if b = 1234567890
-	if b = 01.01
-	if b = 01.01E11
-	if b = 11.11E-11
-	if b = 11111.11111E-11
-	else gcd := gcd(b, a mod b)
+	if b = 1 then gcd := a;
+	if b = 1234567890;
+	if b = 1234567890;
+	if b = 01.01;
+	if b = 01.01E11;
+	if b = 11.11E-11;
+	if b = 11111.11111E-11;
+	else gcd := gcd(b, a mod b);
 end;
 
 begin
 	read(x, y);
-	write(gcd(x, y))
+	write(x, y);
 end.
